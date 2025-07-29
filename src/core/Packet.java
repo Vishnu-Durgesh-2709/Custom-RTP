3b70b2d9b291823b766f273cb2ba19ae0cf4707f
M. Vinay <license@merawla.in>
2025-07-29 21:42:14 +0530

    Deserialize Bytes

--- src/core/Packet.java
+++ src/core/Packet.java
@@ -9,7 +9,7 @@ public class Packet {
     byte flags;
     short windowSize;
 
-    public byte[] toBytes(String message){
+    public byte[] serializeBytes(String message){
         byte[] messageBytes = message.getBytes(StandardCharsets.UTF_8);
         int size = message.length();
         int totalSize = Integer.BYTES + Integer.BYTES + Byte.BYTES + Short.BYTES + Integer.BYTES + messageBytes.length;
@@ -24,5 +24,20 @@ public class Packet {
         buffer.put(messageBytes);
 
         return buffer.array();
+    }
+
+    public String deserializeBytes(byte[] data){
+
+        ByteBuffer buffer = ByteBuffer.wrap(data);
+        int seqNo = buffer.getInt();
+        int ackNo = buffer.getInt();
+        byte flag = buffer.get();
+        short windowSize = buffer.getShort();
+
+        int size = buffer.getInt();
+        byte[] message = new byte[size];
+        buffer.get(message);
+
+        return new String(message, StandardCharsets.UTF_8);
     }
 }
